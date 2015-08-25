0984f64638b7a8ebdb19cee5ecb11157e3c7aab4
VadasD <burgerp@gilmour.org>
2015-08-25 15:05:45 -0400

    1st days work

--- src/ticTacToe/Game.java
+++ src/ticTacToe/Game.java
@@ -5,9 +5,14 @@ public class Game {
 	public static void main(String[] args) {
 /*declares class*/ 
 /*initialize 2d array*/
+		int[][] gameBoard = new int[3][3];
 /*Input array data*/
-/*Check if win condition has been met*/
 /*initialize turn counter*/
+		boolean P1 = true;
+		System.out.println("shall we play a game?");
+/*Check if win condition has been met*/
+		while (gameStatus(gameBoard));
+		print
 /*based on turn counter print array & directions for input P1/P2*/
 /*accept input of correct type*/
 /*check if input is acceptable*/
