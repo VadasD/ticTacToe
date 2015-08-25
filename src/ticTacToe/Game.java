aa4ffc75ebc4a32d436276097c70945b3b868802
VadasD <burgerp@gilmour.org>
2015-08-25 14:46:35 -0400

    created pseudo-code

---
+++ src/ticTacToe/Game.java
@@ -0,0 +1,20 @@
+package ticTacToe;
+
+public class Game {
+
+	public static void main(String[] args) {
+/*declares class*/ 
+/*initialize 2d array*/
+/*Input array data*/
+/*Check if win condition has been met*/
+/*initialize turn counter*/
+/*based on turn counter print array & directions for input P1/P2*/
+/*accept input of correct type*/
+/*check if input is acceptable*/
+/*update array with acceptable input*/
+
+/*if win condition has been met display MSG & end*/
+/*if tie condition has been met display MSG & end*/
+	}
+
+}
