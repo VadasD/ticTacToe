2da18e2fe39cd9e2596ce1d32998cd5b18962bac
VadasD <burgerp@gilmour.org>
2015-09-10 17:35:19 -0400

    final work

--- src/ticTacToe/Game.java
+++ src/ticTacToe/Game.java
@@ -11,6 +11,7 @@ public class Game {
 	public static void main(String[] args) {
 		/* declares class */
 		/* initialize 2d array */
+		int turnCounter = 0;
 		char[][] gameBoard = new char[3][3];
 		/* Input array data */
 		boolean gameStatus;
@@ -19,13 +20,18 @@ public class Game {
 		System.out.println("shall we play a game?");
 
 		/* Check if win condition has been met */
-		while (gameStatus(gameBoard)){
-		printDatBoard(gameBoard);
-		System.out.println("It's " + WhoseTurn(p1) + "'s turn.");
-		tic(gameBoard, p1);
-		p1 = !p1;/* apparently functions as a turn counter */
-		}//closes while loop
-}//closes main method
+		while (gameStatus(gameBoard)) {
+			printDatBoard(gameBoard);
+			System.out.println("It's " + WhoseTurn(p1) + "'s turn.");
+			tic(gameBoard, p1);
+			p1 = !p1;
+			turnCounter++;
+			if((turnCounter)==9){
+				System.out.print("Tied Game");
+				break;}/* apparently functions as a turn counter */
+			
+		}// closes while loop
+	}// closes main method
 
 	/**
 	 * 
@@ -49,23 +55,31 @@ public class Game {
 	 * @return
 	 */
 	public static char[][] tic(char[][] gameBoard, boolean p1) {
-		while(true){
 		Scanner xi = new Scanner(System.in);
-		Scanner yi = new Scanner(System.in);
-		System.out.println("Enter desired X coordinate");
-		int x = xi.nextInt();
-		System.out.println("Enter desired Y coordinate");
-		int y = yi.nextInt();
-		if(p1 && gameBoard[x][y] != 'X'|'O')
-		//if (gameBoard[x][y] == ('X'|'O')){
-			//System.out.println("try again");
-			//return false;}//im trying to check valid moves
-		if (p1) {
-			gameBoard[x][y] = 'X';
-		} else {
-			gameBoard[x][y] = 'O';
-		}
-		return gameBoard;
+		while (true) {
+			System.out.println("Enter desired X coordinate");
+			int x = xi.nextInt();
+			if (x<0 || x>2) {
+				System.out.println("please enter a valid cordinate point (0-2)");
+			} else {
+
+				System.out.println("Enter desired Y coordinate");
+				int y = xi.nextInt();
+				if (y<0 || y>2){
+					System.out.println("please enter a valid cordinate point (0-2)");
+				} else {
+					if (gameBoard[x][y] == 'X' || gameBoard[x][y] == 'O') {
+						System.out.println("try again");
+					} else {
+						if (p1) {
+							gameBoard[x][y] = 'X';
+						} else {
+							gameBoard[x][y] = 'O';
+						}
+						return gameBoard;
+					}
+				}
+			}
 		}
 	} // closes tic method
 
@@ -87,7 +101,7 @@ public class Game {
 		for (int x = 0; x < 3; x++) { // check for vertical pattern
 			for (int y = 0; y < 3; y++) {
 				current = gameBoard[x][y];
-				if (last == current && last != 0) {
+				if (last == current && current != 0) {
 					counter++; // keep track of how many in a row we've found
 				}
 				if (counter == 2) {
@@ -97,11 +111,12 @@ public class Game {
 				last = current;
 			}
 		}
-		counter = 0; // reset the counter back to 0 before the next check
+		counter = 0; 
+		last = '1';// reset the counter back to 0 before the next check
 		for (int y = 0; y < 3; y++) { // check for a horizontal pattern
 			for (int x = 0; x < 3; x++) {
 				current = gameBoard[x][y];
-				if (last == current && last != 0) {
+				if (last == current && current != 0) {
 					counter++;
 				}
 				if (counter == 2) {
@@ -111,10 +126,11 @@ public class Game {
 				last = current;
 			}
 		}
-		counter = 0; // reset the counter back to 0 before the next check
+		counter = 0; 
+		last = '1'; // reset the counter back to 0 before the next check
 		for (int y = 0; y < 3; y++) { // check for a diagnal down pattern
 			current = gameBoard[y][y];
-			if (last == current && last != 0) {
+			if (last == current && current != 0) {
 				counter++;
 			}
 			if (counter == 2) {
@@ -123,10 +139,11 @@ public class Game {
 			}
 			last = current;
 		}
-		counter = 0; // reset the counter back to 0 before the next check
+		counter = 0; 
+		last = '1';// reset the counter back to 0 before the next check
 		for (int x = 0; x < 3; x++) { // check for a diagnal up pattern
 			current = gameBoard[x][yDiag];
-			if (last == current && last != 0) {
+			if (last == current && current != 0) {
 				counter++;
 			}
 			if (counter == 2) {
@@ -138,15 +155,16 @@ public class Game {
 		}
 		return true;
 	}// closes gameStatus method
-		public static String WhoseTurn(boolean p1) { // handy little method to
-			// return a string of
-			// who's turn it is.
-if (p1) {
-return "Player 1";
-} else {
-return "Player 2";
-	}
+
+	public static String WhoseTurn(boolean p1) { // handy little method to
+		// return a string of
+		// who's turn it is.
+		if (p1) {
+			return "Player 1";
+		} else {
+			return "Player 2";
 		}
+	}
 }// closes Game class
 
 /* if tie condition has been met display MSG & end */
--- src/ticTacToe/adilleta.java
+++ src/ticTacToe/adilleta.java
@@ -1,120 +1,63 @@
-package ticTacToe;
-	import java.util.Scanner; //allows me to take in input
+public class Converter { 
 
-	/**
-	 * TIC TAC TOE
-	 * 
-	 * @author adilettad
-	 */
-	public class adilleta {
+   // converts integer n into a base b string
+    public static String toString(int n, int base) {
+       // special case
+       if (n == 0) return "0";
 
-		public static void main(String[] args) {
-			int[][] gameBoard = new int[3][3]; // 3x3 array for the game board
-			boolean player1 = true; // keeps track of whose turn it is
-			System.out.println("Let's play"); // welcome message
-			while (CheckWinner(gameBoard)) { // start game loop
-				GameBoardPrint(gameBoard); // print the current status
-				System.out.println("It's " + WhoseTurn(player1) + "'s turn.");
-				gameBoard = Tic(gameBoard, player1); // input move
-				player1 = !player1; // switch whose turn it is
-			}
-		}
+       String digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       String s = "";
+       while (n > 0) {
+          int d = n % base;
+          s = digits.charAt(d) + s;
+          n = n / base;
+       }
+       return s;
+    }
 
-		public static void GameBoardPrint(int[][] gameBoard) {
-			String newLine = System.getProperty("line.separator");// This will
-																	// retrieve line
-																	// separator
-																	// dependent on
-																	// OS.
-			for (int y = 0; y < 3; y++) { // printing vertically
-				for (int x = 0; x < 3; x++) {
-					System.out.print("[ " + gameBoard[x][y] + " ]");
-				}
-				System.out.print(newLine); // create a new line
-			}
-		}
+    public static String toBinaryString(int n) { return toString(n,  2); }
+    public static String toHexString(int n)    { return toString(n, 16); }
 
-		public static int[][] Tic(int[][] gameBoard, Boolean player1) {
-			// TODO: subtract 1 from inputs so they adjust to array's 0 start point
-			Scanner xi = new Scanner(System.in); // store the x coordinate
-			Scanner yi = new Scanner(System.in); // store the y coordinate
-			System.out.println("Please enter your desired X coordinate");
-			int x = xi.nextInt(); // TODO needs execption handling
-			System.out.println("Please enter your desired Y coordinate");
-			int y = yi.nextInt(); // TODO needs execption handling
-			if (player1) {
-				gameBoard[x][y] = 1;
-			} else {
-				gameBoard[x][y] = 2;
-			}
-			return gameBoard;
-		}
 
-		public static boolean CheckWinner(int[][] gameBoard) {
-			int current;
-			int last = 0;
-			int counter = 0;
-			int yDiag = 2;
-			for (int x = 0; x < 3; x++) { // check for vertical pattern
-				for (int y = 0; y < 3; y++) {
-					current = gameBoard[x][y];
-					if (last == current && last != 0) {
-						counter++; // keep track of how many in a row we've found
-					}
-					if (counter == 3) {
-						System.out.println("You won!");
-						return false;
-					}
-					last = current;
-				}
-			}
-			for (int y = 0; y < 3; y++) { // check for a horizontal pattern
-				for (int x = 0; x < 3; x++) {
-					current = gameBoard[x][y];
-					if (last == current && last != 0) {
-						counter++;
-					}
-					if (counter == 3) {
-						System.out.println("You won!");
-						return false;
-					}
-					last = current;
-				}
-			}
-			for (int y = 0; y < 3; y++) { // check for a diagnal down pattern
-				current = gameBoard[y][y];
-				if (last == current && last != 0) {
-					counter++;
-				}
-				if (counter == 3) {
-					System.out.println("You won!");
-					return false;
-				}
-				last = current;
-			}
-			for (int x = 0; x < 3; x++) { // check for a diagnal up pattern
-				current = gameBoard[x][yDiag];
-				if (last == current && last != 0) {
-					counter++;
-				}
-				if (counter == 3) {
-					System.out.println("You won!");
-					return false;
-				}
-				last = current;
-				yDiag--;
-			}
-			return true;
-		}
+    public static void inputError(String s) {
+        throw new RuntimeException("Input error with" + s);
+    }
 
-		public static String WhoseTurn(boolean player1) { // handy little method to
-															// return a string of
-															// who's turn it is.
-			if (player1) {
-				return "Player 1";
-			} else {
-				return "Player 2";
-			}
-		}
-	}
+    // convert a String representing a base b integer into an int
+    public static int fromString(String s, int b) {
+       int result = 0;
+       int digit = 0;
+       for (int i = 0; i < s.length(); i++) {
+          char c = s.charAt(i);
+          if (c >= '0' && c <= '9')
+             digit = c - '0';
+          else if (c >= 'A' && c <= 'Z')
+             digit = 10 + c - 'A';
+          else inputError(s);
 
+          if (digit < b) result = b * result + digit;
+          else inputError(s);
+       }
+       return result;
+    }
+
+    public static int fromBinaryString(String s) { return fromString(s,  2); }
+    public static int fromHexString(String s)    { return fromString(s, 16); }
+
+
+    // sample test client
+    public static void main(String[] args) {
+        int n = 47;
+        String s = "101111";
+        System.out.println(n +  ":  " + toBinaryString(n));
+        System.out.println(s +  ":  " + fromBinaryString(s));
+        System.out.println(n +  ":  " + toHexString(n));
+        System.out.println();
+
+        n = 2620;
+        s = "0A3C";
+        System.out.println(n +  ":  " + toBinaryString(n));
+        System.out.println(s +  ":  " + fromHexString(s));
+        System.out.println(n +  ":  " + toHexString(n));
+    }
+}
